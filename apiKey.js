1cd89c61c4dd1be979d73429154842172b0bd960
Steve yoes <syoes03@gmail.com>
2020-01-03 22:38:38 -0800

    Delete apiKey.js

--- apiKey.js
+++
@@ -1,6 +0,0 @@
-// The Halo 5 API key, can be found here: https://developer.haloapi.com/products/560af1e42109182040fb56fc
-// Just replace the placeholder below with your key.
-let apiKey = "28f0170248924daabed8a5f969e954d6";
-module.exports = { apiKey };
-
-//module.exports = { apiKey: "28f0170248924daabed8a5f969e954d6" };
